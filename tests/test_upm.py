2cc22b263e3b1b5e844a36db275f00e9e96c1815
colin <colinpfarrell@gmail.com>
2018-10-01 20:06:29 -0700

    Testing Bug

--- tests/test_upm.py
+++ tests/test_upm.py
@@ -1,15 +1,17 @@
 import unittest
+import os
 import pandas as pd
 import numpy as np
 from UPMRun import MethylationEM
 from UPMHelpers import import_upm_table
 
-test_data_folder = 'tests/test_data/'
-test_input_file_path = f'{test_data_folder}meth-tab-n100-m200.txt'
-control_rates = f'{test_data_folder}meth-tab-n100-m200.txt.CEM-rates-MCvsPM.csv'
-control_ages = f'{test_data_folder}meth-tab-n100-m200.txt.CEM-ages-MCvsPM.csv'
+test_data_folder = dir_path = os.path.dirname(os.path.realpath(__file__))
+test_input_file_path = f'{test_data_folder}/test_data/meth-tab-n100-m200.txt'
+control_rates = f'{test_data_folder}/test_data/meth-tab-n100-m200.txt.CEM-rates-MCvsPM.csv'
+control_ages = f'{test_data_folder}/test_data/meth-tab-n100-m200.txt.CEM-times-MCvsPM.csv'
 
 test_samples, test_ages, test_meth_sits, test_meth_table = import_upm_table(upm_file=test_input_file_path)
+
 
 test_em = MethylationEM(methylation_table=test_meth_table,
                         sample_list=test_samples,
@@ -17,11 +19,10 @@ test_em = MethylationEM(methylation_table=test_meth_table,
                         times=test_ages,
                         iter_limit=100,
                         err_tolerance=0.00001,
-                        output_path='tests/test_data/test_output')
+                        output_path=f'{test_data_folder}/test_data/test_output')
 
-control_age_df = pd.read_csv(control_ages, sep=',', header=True, index_col=0)
-control_rate_df = pd.read_csv(control_rates, sep=',', header=True, index_col=0)
-
+control_age_df = pd.read_csv(control_ages, sep=',', index_col=0)
+control_rate_df = pd.read_csv(control_rates, sep=',', index_col=0)
 
 class TestUPM(unittest.TestCase):
 
@@ -34,6 +35,7 @@ class TestUPM(unittest.TestCase):
 
     def test_pm_rates(self):
         for control_rate, test_rate in zip(list(control_rate_df['PM-rate']), test_em.UPM_EC_EM_results['PM_rates']):
+            print(control_rate, test_rate)
             assert np.isclose(control_rate, test_rate)
 
     def test_rate_len(self):
