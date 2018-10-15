745ace1a8aaa12812db3c7c4c54bb7a37ffd1e07
NuttyLogic <colinpfarrell@gmail.com>
2018-10-15 10:58:14 -0700

    Cleaned UPM Code Base - Stable Code - Simple Testing Structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# EpigeneticClock_UniversalPaceMaker
-Epigenetic Clock - Universal Pace Maker Hill Climbing Code Base
--- tests/test_upm.py
+++ tests/test_upm.py
@@ -1,26 +1,28 @@
 import unittest
+import os
 import pandas as pd
-import numpy as np
+import math
 from UPMRun import MethylationEM
 from UPMHelpers import import_upm_table
 
-test_data_folder = 'tests/test_data/'
-test_input_file_path = f'{test_data_folder}meth-tab-n100-m200.txt'
-control_rates = f'{test_data_folder}meth-tab-n100-m200.txt.CEM-rates-MCvsPM.csv'
-control_ages = f'{test_data_folder}meth-tab-n100-m200.txt.CEM-ages-MCvsPM.csv'
+test_data_folder = dir_path = os.path.dirname(os.path.realpath(__file__))
+test_input_file_path = f'{test_data_folder}/test_data/meth-tab-n100-m200.txt'
+control_rates = f'{test_data_folder}/test_data/meth-tab-n100-m200.txt.CEM-rates-MCvsPM.csv'
+control_ages = f'{test_data_folder}/test_data/meth-tab-n100-m200.txt.CEM-times-MCvsPM.csv'
 
-test_samples, test_ages, test_meth_sits, test_meth_table = import_upm_table(upm_file=test_input_file_path)
+test_samples, test_ages, test_meth_sites, test_meth_table = import_upm_table(upm_file=test_input_file_path)
+
 
 test_em = MethylationEM(methylation_table=test_meth_table,
                         sample_list=test_samples,
-                        site_list=test_meth_sits,
+                        site_list=test_meth_sites,
                         times=test_ages,
                         iter_limit=100,
                         err_tolerance=0.00001,
-                        output_path='tests/test_data/test_output')
+                        output_path=f'{test_data_folder}/test_data/test_output')
 
-control_age_df = pd.read_csv(control_ages, sep=',', header=True, index_col=0)
-control_rate_df = pd.read_csv(control_rates, sep=',', header=True, index_col=0)
+control_age_df = pd.read_csv(control_ages, sep=',', index_col=0)
+control_rate_df = pd.read_csv(control_rates, sep=',', index_col=0)
 
 
 class TestUPM(unittest.TestCase):
@@ -30,11 +32,11 @@ class TestUPM(unittest.TestCase):
 
     def test_pm_ages(self):
         for control_age, test_age in zip(list(control_age_df['PM-age']), test_em.UPM_EC_EM_results['PM_times']):
-            assert np.isclose(control_age, test_age)
+            assert math.isclose(control_age, test_age, rel_tol=0.001)
 
     def test_pm_rates(self):
         for control_rate, test_rate in zip(list(control_rate_df['PM-rate']), test_em.UPM_EC_EM_results['PM_rates']):
-            assert np.isclose(control_rate, test_rate)
+            assert math.isclose(control_rate, test_rate, rel_tol=0.001)
 
     def test_rate_len(self):
         self.assertEqual(len(list(control_age_df['PM-age'])), len(test_em.sample_list))
