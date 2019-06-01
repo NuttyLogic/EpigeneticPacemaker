52ac74d893faa998dc38402c7e4fa2ffa9ba0241
nuttylogic <colinpfarrell@gmail.com>
2019-05-31 17:11:44 -0700

    Cross validation bug

--- Tests/test_upm_cv.py
+++ Tests/test_upm_cv.py
@@ -14,12 +14,12 @@ control_ages = f'{test_data_folder}/test_data/meth-tab-n100-m200.txt.CEM-times-M
 test_samples, test_ages, test_meth_sites, test_meth_table = import_upm_table(upm_file=test_input_file_path)
 
 
-all_samples = MethylationEM(methylation_table=test_meth_table,
+all_samples = MethylationEM(methylation_array=test_meth_table,
                             sample_list=test_samples,
                             site_list=test_meth_sites,
-                            times=test_ages,
+                            states=test_ages,
                             iter_limit=100,
-                            err_tolerance=0.00001)
+                            error_tolerance=0.00001)
 
 # test age prediction
 sample_0_age = predict_age(np.asarray(test_meth_table)[:, 0:1],
